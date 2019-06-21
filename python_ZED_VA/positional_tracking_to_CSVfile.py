4639bb4de392538a4e223ff51b3d5b38a1b6367a
Virginia Pan <vgp4@duke.edu>
2019-06-21 16:13:47 -0400

    print camera position info to file on SD card for 100sec

---
+++ python_ZED_VA/positional_tracking_to_CSVfile.py
@@ -0,0 +1,86 @@
+########################################################################
+#
+# Copyright (c) 2017, STEREOLABS.
+#
+# All rights reserved.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+########################################################################
+
+import pyzed.sl as sl
+
+def main():
+    # Create a Camera object
+    zed = sl.Camera()
+
+    # Create a InitParameters object and set configuration parameters
+    init_params = sl.InitParameters()
+    init_params.camera_resolution = sl.RESOLUTION.RESOLUTION_HD720  # Use HD720 video mode (default fps: 60)
+    # Use a right-handed Y-up coordinate system
+    init_params.coordinate_system = sl.COORDINATE_SYSTEM.COORDINATE_SYSTEM_RIGHT_HANDED_Y_UP
+    init_params.coordinate_units = sl.UNIT.UNIT_METER  # Set units in meters
+
+    # Open the camera
+    err = zed.open(init_params)
+    if err != sl.ERROR_CODE.SUCCESS:
+        exit(1)
+
+    # Enable positional tracking with default parameters
+    py_transform = sl.Transform()  # First create a Transform object for TrackingParameters object
+    tracking_parameters = sl.TrackingParameters(init_pos=py_transform)
+    err = zed.enable_tracking(tracking_parameters)
+    if err != sl.ERROR_CODE.SUCCESS:
+        exit(1)
+
+    # Track the camera position during 1000 frames
+    i = 0
+    zed_pose = sl.Pose()
+    zed_imu = sl.IMUData()
+    runtime_parameters = sl.RuntimeParameters()
+
+    #added! 
+    path = '/media/nvidia/SD1/position.csv'
+    position_file = open(path,'w')
+    
+    while i < 1000:
+        if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+            # Get the pose of the left eye of the camera with reference to the world frame
+            zed.get_position(zed_pose, sl.REFERENCE_FRAME.REFERENCE_FRAME_WORLD)
+            zed.get_imu_data(zed_imu, sl.TIME_REFERENCE.TIME_REFERENCE_IMAGE)
+
+            # Display the translation and timestamp
+            py_translation = sl.Translation()
+            tx = round(zed_pose.get_translation(py_translation).get()[0], 3)
+            ty = round(zed_pose.get_translation(py_translation).get()[1], 3)
+            tz = round(zed_pose.get_translation(py_translation).get()[2], 3)
+            position_file.write("Translation: Tx: {0}, Ty: {1}, Tz {2}, Timestamp: {3}\n".format(tx, ty, tz, zed_pose.timestamp))
+
+            # Display the orientation quaternion
+            py_orientation = sl.Orientation()
+            ox = round(zed_pose.get_orientation(py_orientation).get()[0], 3)
+            oy = round(zed_pose.get_orientation(py_orientation).get()[1], 3)
+            oz = round(zed_pose.get_orientation(py_orientation).get()[2], 3)
+            ow = round(zed_pose.get_orientation(py_orientation).get()[3], 3)
+            position_file.write("Orientation: Ox: {0}, Oy: {1}, Oz {2}, Ow: {3}\n".format(ox, oy, oz, ow))
+            
+            i = i + 1
+
+    # Close the camera
+    zed.close()
+
+    # Close file
+    position_file.close()
+
+if __name__ == "__main__":
+    main()
